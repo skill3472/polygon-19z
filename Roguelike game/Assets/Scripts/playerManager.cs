c2aaa5b87c28b82a1fad294b0d8b6ec7758562ac
Maksymilian Tym <tymszczur@gmail.com>
2019-11-20 18:54:03 +0100

    Added player and enemy movement

---
+++ Roguelike game/Assets/Scripts/playerManager.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class playerManager : MonoBehaviour
+{
+
+	private float horizontalAxis;
+	private float verticalAxis;
+	[HideInInspector]public bool isMoving;
+	[Header("Movement Settings")]
+	[SerializeField]private float movementSpeed;
+	[SerializeField]private Rigidbody2D rb;
+
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        Move();
+    }
+
+    void Move()
+    {
+    	horizontalAxis = Input.GetAxis("Horizontal");
+    	verticalAxis = Input.GetAxis("Vertical");
+
+    	rb.velocity = new Vector2(horizontalAxis * movementSpeed, verticalAxis * movementSpeed);
+	}
+}
---
+++ Roguelike game/Assets/Scripts/simplestEnemyAI.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D))]
+public class simplestEnemyAI : MonoBehaviour
+{
+
+	[Header("AI Settings")]
+	[SerializeField]private Transform targetPlayer;
+	[SerializeField]private float rotationSpeed;
+	[SerializeField]private float movementSpeed;
+	[SerializeField]private Rigidbody2D rb;
+    
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        MoveTowardsPlayer();
+    }
+
+    void MoveTowardsPlayer()
+    {
+    	Vector3 vectorToTarget = targetPlayer.position - transform.position;
+ 		float angle = Mathf.Atan2(vectorToTarget.y, vectorToTarget.x) * Mathf.Rad2Deg;
+ 		Quaternion q = Quaternion.AngleAxis(angle, Vector3.forward);
+ 		transform.rotation = Quaternion.Slerp(transform.rotation, q, Time.deltaTime * rotationSpeed);
+
+ 		float step = movementSpeed * Time.deltaTime;
+
+        if(Vector3.Distance(transform.position, targetPlayer.position) > 2f)
+        transform.position = Vector2.MoveTowards(transform.position, targetPlayer.position, step);
+        else if(Vector3.Distance(transform.position, targetPlayer.position) < 2f)
+        transform.position = Vector2.MoveTowards(transform.position, targetPlayer.position, -step);
+        else
+        transform.position = transform.position;
+    }
+}
