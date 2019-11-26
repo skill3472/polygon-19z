1092844caec7fe90cb91693c97b28365c63b2280
Adam Madela <dungald@gmail.com>
2019-11-26 07:32:48 +0100

    ruch camery po wejsciu na portal v1

---
+++ Roguelike game/Assets/PortalManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PortalManager : MonoBehaviour
+{
+    public GameObject room1;
+    public GameObject room2;
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.Equals(GameObject.Find("Player").GetComponent<Collider2D>()))
+        {
+            Debug.Log("Player Enter Portal");
+            // TODO zmienic na odpowiedni pokoj zamiast na this (Portal) bez mnoznika
+            GameObject camera = GameObject.Find("Main Camera");
+            camera.transform.position = Vector3.Lerp(camera.transform.position, new Vector3(this.transform.position.x*2, this.transform.position.y*2, camera.transform.position.z), 3f);
+        }
+        
+    }
+}
