ad637eb400cd6c3e4ef4842f6755310406719aba
Adam Madela <dungald@gmail.com>
2019-11-26 22:51:09 +0100

    wyjscie i powort do pomieszczenia

--- Roguelike game/Assets/PortalManager.cs
+++ Roguelike game/Assets/PortalManager.cs
@@ -8,13 +8,19 @@ public class PortalManager : MonoBehaviour
     public GameObject room2;
 
     private void OnTriggerEnter2D(Collider2D other)
-    {
-        if (other.Equals(GameObject.Find("Player").GetComponent<Collider2D>()))
+    {   
+        if ((other.Equals(GameObject.Find("Player").GetComponent<Collider2D>()) && room1 != null && room2 != null))
         {
             Debug.Log("Player Enter Portal");
-            // TODO zmienic na odpowiedni pokoj zamiast na this (Portal) bez mnoznika
             GameObject camera = GameObject.Find("Main Camera");
-            camera.transform.position = Vector3.Lerp(camera.transform.position, new Vector3(this.transform.position.x*2, this.transform.position.y*2, camera.transform.position.z), 3f);
+            if (camera.transform.position.x.Equals(room1.transform.position.x) && camera.transform.position.y.Equals(room1.transform.position.y))
+            {
+                camera.transform.position = Vector3.Lerp(camera.transform.position, new Vector3(room2.transform.position.x, room2.transform.position.y, camera.transform.position.z), 3f);
+            } else
+            {
+                camera.transform.position = Vector3.Lerp(camera.transform.position, new Vector3(room1.transform.position.x, room1.transform.position.y, camera.transform.position.z), 3f);
+            }
+            
         }
         
     }
--- Roguelike game/Assets/Scripts/Utils/RoomGenerator.cs
+++ Roguelike game/Assets/Scripts/Utils/RoomGenerator.cs
@@ -22,44 +22,81 @@ public class RoomGenerator : MonoBehaviour
 
         for (int i = 0; possibleRooms.Length > i; i++)
         {
+            if ((i == 1 || i == 3 || i == 5 || i == 7) && possibleRooms[i] != 1)
+            {
+                continue;
+            }
+            else if (i == 0 && (possibleRooms[1] + possibleRooms[3] == 0))
+            {
+                continue;
+            }
+            else if (i == 2 && (possibleRooms[1] + possibleRooms[5] == 0))
+            {
+                continue;
+            }
+            else if (i == 6 && (possibleRooms[3] + possibleRooms[7] == 0))
+            {
+                continue;
+            }
+            else if (i == 8 && (possibleRooms[5] + possibleRooms[7] == 0))
+            {
+                continue;
+            }
+
             currentRooms[i] = new GameObject("room " + i);
-            if (i == 4)
-            {
-                Instantiate(topWall, currentRooms[i].transform);
-                Instantiate(rightWall, currentRooms[i].transform);
-                Instantiate(bottomWall, currentRooms[i].transform);
-                Instantiate(leftWall, currentRooms[i].transform);
-            }
-             else if (i == 1 || i == 3 || i == 5 || i == 7)
-            {
-                if (possibleRooms[i] != 1)
-                {
-                    continue;
-                }
-            }
-            else
-            {
-                if (i == 0 && (possibleRooms[1] + possibleRooms[3] == 0))
-                {
-                    continue;
-                }
-                if (i == 2 && (possibleRooms[1] + possibleRooms[5] == 0))
-                {
-                    continue;
-                }
-                if (i == 6 && (possibleRooms[3] + possibleRooms[7] == 0))
-                {
-                    continue;
-                }
-                if (i == 8 && (possibleRooms[5] + possibleRooms[7] == 0))
-                {
-                    continue;
-                }
-            }
-
             currentRooms[i].transform.parent = this.transform;
+            if (i ==4)
+            {
+                currentRooms[i].transform.position = new Vector3(currentRooms[i].transform.position.x, currentRooms[i].transform.position.y, 0);
+            }
 
+            if (i == 0 || i == 1 || i == 2)
+            {
+                currentRooms[i].transform.position = new Vector3(currentRooms[i].transform.position.x, currentRooms[i].transform.position.y + 9.4f, 0);
+            }
 
+            if (i == 0 || i == 3 || i == 6)
+            {
+                currentRooms[i].transform.position = new Vector3(currentRooms[i].transform.position.x - 17.1f, currentRooms[i].transform.position.y, 0);
+            }
+
+            if (i == 6 || i == 7 || i == 8)
+            {
+                currentRooms[i].transform.position = new Vector3(currentRooms[i].transform.position.x, currentRooms[i].transform.position.y - 9.4f, 0);
+            }
+
+            if (i == 2 || i == 5 || i == 8)
+            {
+                currentRooms[i].transform.position = new Vector3(currentRooms[i].transform.position.x + 17.1f, currentRooms[i].transform.position.y, 0);
+            }
+        }
+
+        GameObject topWallInstance = Instantiate(topWall, currentRooms[4].transform);
+        topWallInstance.transform.GetChild(2).GetComponent<PortalManager>().room1 = currentRooms[4];
+        if (currentRooms[1] != null)
+        {
+            topWallInstance.transform.GetChild(2).GetComponent<PortalManager>().room2 = currentRooms[1];
+        }
+
+        GameObject rightWallInstance = Instantiate(rightWall, currentRooms[4].transform);
+        rightWallInstance.transform.GetChild(1).GetComponent<PortalManager>().room1 = currentRooms[4];
+        if (currentRooms[5] != null)
+        {
+            rightWallInstance.transform.GetChild(1).GetComponent<PortalManager>().room2 = currentRooms[5];
+        }
+
+        GameObject bottomWallInstance = Instantiate(bottomWall, currentRooms[4].transform);
+        bottomWallInstance.transform.GetChild(2).GetComponent<PortalManager>().room1 = currentRooms[4];
+        if (currentRooms[7] != null)
+        {
+            bottomWallInstance.transform.GetChild(2).GetComponent<PortalManager>().room2 = currentRooms[7];
+        }
+
+        GameObject leftWallInstance = Instantiate(leftWall, currentRooms[4].transform);
+        leftWallInstance.transform.GetChild(1).GetComponent<PortalManager>().room1 = currentRooms[4];
+        if (currentRooms[3] != null)
+        {
+            leftWallInstance.transform.GetChild(1).GetComponent<PortalManager>().room2 = currentRooms[3];
         }
     }
 
