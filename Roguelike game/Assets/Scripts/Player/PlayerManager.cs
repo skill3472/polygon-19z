68f6a846773b065511a70d53f03587bb15e74804
Maksymilian Tym <tymszczur@gmail.com>
2019-11-27 09:18:44 +0100

    Fixed sum formatting

--- Roguelike game/Assets/Scripts/Player/PlayerManager.cs
+++ Roguelike game/Assets/Scripts/Player/PlayerManager.cs
@@ -30,7 +30,7 @@ public class PlayerManager : MonoBehaviour
     {
         weaponSlot = Instantiate(weaponList[0], this.gameObject.transform);
         weaponSlot.name = "0";
-    } 
+    }
 
     void WeaponSwitch()
     {
@@ -68,7 +68,7 @@ public class PlayerManager : MonoBehaviour
     void Attack()
     {
 
-            if (Input.GetButtonDown("Fire3"))
+        if (Input.GetButtonDown("Fire3"))
         {
             weaponSlot.GetComponent<Attack>().Second();
         }
