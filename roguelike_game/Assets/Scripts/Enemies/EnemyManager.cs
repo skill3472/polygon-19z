d7d27fba8968d5673bce47f8e9e66f69eb57c0a5
Maksymilian Tym <tymszczur@gmail.com>
2019-11-30 20:03:53 +0100

    Damage flash made by Mango
    Co-Authored-By: null <YgnisAvi@users.noreply.github.com>

--- roguelike_game/Assets/Scripts/Enemies/EnemyManager.cs
+++ roguelike_game/Assets/Scripts/Enemies/EnemyManager.cs
@@ -1,4 +1,4 @@
-﻿using System.Collections;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -6,13 +6,30 @@ public class EnemyManager : MonoBehaviour
 {
     public int enemyHealth;
     public int enemyDamageOutput;
+    [SerializeField]private float damageFlashDuration;
+
+    [SerializeField]private Sprite enemyNormalSprite;
+    [SerializeField]private Sprite enemyFlashingSprite;
+
+    [SerializeField]private float damageFlashTimer;
 
     public void TakeDamage(int weaponDamage)
     {
+        damageFlashTimer = 0;
         enemyHealth -= weaponDamage;
+        gameObject.GetComponent<SpriteRenderer>().sprite = enemyFlashingSprite;
 
         if (enemyHealth <= 0)
             EnemyDeath();
+    }
+
+    void Update()
+    {
+        damageFlashTimer += Time.deltaTime;
+        if (damageFlashTimer >= damageFlashDuration) {
+            damageFlashTimer = 0;
+            gameObject.GetComponent<SpriteRenderer>().sprite = enemyNormalSprite;
+        }
     }
 
     void EnemyDeath()
