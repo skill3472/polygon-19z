0fb058b283d3ce720c2b1f888213036f10a5a838
Adam Madela <dungald@gmail.com>
2019-12-01 15:04:50 +0100

    spawn przeciwnikow na gridzie

---
+++ roguelike_game/Assets/Scripts/World/SpawnGridManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnGridManager : MonoBehaviour
+{
+    public GameObject[] enemyList;
+    public GameObject[] itemList;
+    private int[,] grid = new int[12, 6];
+    // Start is called before the first frame update
+    void Start()
+    {
+        // 1 to 4 enemies
+        int numberOfEnemies = Random.Range(1, 5);
+        for (int i = 1; i <= numberOfEnemies; i++)
+        {
+            int x = Random.Range(0, 13);
+            int y = Random.Range(0, 7);
+            if (grid[x,y] == 0)
+            {
+                Instantiate(enemyList[0], this.transform.GetChild(x).transform.GetChild(y));
+                grid[x, y] = 1;
+            }
+
+        } 
+    }
+}
