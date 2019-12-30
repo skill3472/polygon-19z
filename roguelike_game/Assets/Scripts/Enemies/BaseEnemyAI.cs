d67bd4f5a2d09d829de3aeea61a4853da157d1ff
Adam Madela <dungald@gmail.com>
2019-12-30 09:05:56 +0100

    refactor AI

---
+++ roguelike_game/Assets/Scripts/Enemies/BaseEnemyAI.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public abstract class BaseEnemyAi : MonoBehaviour
+{
+    [Header("AI Settings")]
+    protected Transform targetPlayer;
+    [SerializeField]
+    [Tooltip("How fast the AI will rotate?")]
+    protected float rotationSpeed;
+    [SerializeField]
+    [Tooltip("How fast will the AI walk?")]
+    protected float movementSpeed;
+    [SerializeField]
+    [Range(0f, 10f)]
+    [Tooltip("How far from the player will the AI stop walking and start shooting?")]
+    protected float safePlayerDistance;
+    [SerializeField]
+    [Range(0f, 10f)]
+    [Tooltip("How far from the player will the AI start walking to player")]
+    protected float detectionPlayerDistance;
+    [SerializeField]
+    [Tooltip("The AI's Rigidbody2D component")]
+    protected Rigidbody2D rb;
+
+    void Start()
+    {
+        GameObject player = GameObject.Find("Player");
+        if (player != null)
+        {
+            targetPlayer = player.transform;
+        }
+    }
+
+    void Update()
+    {
+        Behavior();
+    }
+
+    protected abstract void Behavior();
+}
--- roguelike_game/Assets/Scripts/Enemies/SimplestEnemyAI.cs
+++ roguelike_game/Assets/Scripts/Enemies/SimplestEnemyAI.cs
@@ -3,36 +3,10 @@ using System.Collections.Generic;
 using UnityEngine;
 
 [RequireComponent(typeof(Rigidbody2D))]
-public class SimplestEnemyAI : MonoBehaviour
+public class SimplestEnemyAI : BaseEnemyAi
 {
 
-	[Header("AI Settings")]
-    private Transform targetPlayer;
-	[SerializeField][Tooltip("How fast the AI will rotate?")]
-    private float rotationSpeed;
-	[SerializeField][Tooltip("How fast will the AI walk?")]
-    private float movementSpeed;
-    [SerializeField][Range(0f,10f)][Tooltip("How far from the player will the AI stop walking and start shooting?")]
-    private float safePlayerDistance;
-    [SerializeField][Range(0f, 10f)][Tooltip("How far from the player will the AI start walking to player")]
-    private float detectionPlayerDistance;
-    [SerializeField][Tooltip("The AI's Rigidbody2D component")]
-    private Rigidbody2D rb;
-    
-    void Start()
-    {
-        GameObject player = GameObject.Find("Player");
-        if (player != null) {
-            targetPlayer = player.transform;
-        }
-    }
-
-    void Update()
-    {
-        MoveTowardsPlayer();
-    }
-
-    void MoveTowardsPlayer()
+    protected override void Behavior()
     {
  		float step = movementSpeed * Time.deltaTime;
 
