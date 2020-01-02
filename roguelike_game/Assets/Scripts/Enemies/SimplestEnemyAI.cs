25479296b632ea5f48efccda3a0fc77d994a22a7
Maksymilian Tym <tymszczur@gmail.com>
2020-01-02 14:55:43 +0100

    Zepsute szczelanie enemy 1

--- roguelike_game/Assets/Scripts/Enemies/SimplestEnemyAI.cs
+++ roguelike_game/Assets/Scripts/Enemies/SimplestEnemyAI.cs
@@ -5,14 +5,22 @@ using UnityEngine;
 [RequireComponent(typeof(Rigidbody2D))]
 public class SimplestEnemyAI : BaseEnemyAi
 {
+    [SerializeField]private GameObject projectilePrefab;
+    [SerializeField]private float projectileSpeed;
 
     protected override void Behavior()
     {
  		float step = movementSpeed * Time.deltaTime;
+        private GameObject projectile;
 
         if(Vector3.Distance(transform.position, targetPlayer.position) > safePlayerDistance && detectionPlayerDistance > Vector3.Distance(transform.position, targetPlayer.position))
-        transform.position = Vector2.MoveTowards(transform.position, targetPlayer.position, step);
+            transform.position = Vector2.MoveTowards(transform.position, targetPlayer.position, step);
         else if(Vector3.Distance(transform.position, targetPlayer.position) < safePlayerDistance - 0.2f)
-        transform.position = Vector2.MoveTowards(transform.position, targetPlayer.position, -step);
+            transform.position = Vector2.MoveTowards(transform.position, targetPlayer.position, -step);
+        else
+        {
+            projectile = Instantiate(projectilePrefab, transform.position, transform.rotation);
+            projectile.GetComponent<Rigidbody2D>().AddForce(Vector2.MoveTowards(transform.position, targetPlayer.position, 10f).Normalize() * projectileSpeed);
+        }
     }
 }
