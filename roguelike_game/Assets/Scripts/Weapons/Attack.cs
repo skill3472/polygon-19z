80da30438d66f5cebe0c8e65dbcfcad825ed5409
Maksymilian Tym <tymszczur@gmail.com>
2020-01-02 12:54:13 +0100

    Added a functional weapon cooldown

--- roguelike_game/Assets/Scripts/Weapons/Attack.cs
+++ roguelike_game/Assets/Scripts/Weapons/Attack.cs
@@ -2,10 +2,11 @@
 
 public abstract class Attack : MonoBehaviour
 {
-    [SerializeField] public int damage;
-    [SerializeField] public string damageType;
-    [SerializeField] public float attackRate;
-    [SerializeField] public bool malee;
+    public int damage;
+    public string damageType;
+    public float attackRate;
+    public bool malee;
+    public float timeLeftToAttack;
 
     public abstract void First();
     public abstract void Second();
--- roguelike_game/Assets/Scripts/Weapons/WeaponCollisionManager.cs
+++ roguelike_game/Assets/Scripts/Weapons/WeaponCollisionManager.cs
@@ -7,6 +7,7 @@ public class WeaponCollisionManager : MonoBehaviour
 {
 
 	private PlayerManager playerManager;
+	[SerializeField]private SwordAttack attackScript;
 
     // Start is called before the first frame update
     void Start()
@@ -17,18 +18,21 @@ public class WeaponCollisionManager : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+		attackScript.timeLeftToAttack += Time.deltaTime;
     }
 
     void OnTriggerEnter2D(Collider2D col)
-    {   
-         
-        Attack attackScript = this.GetComponent<Attack>();
-        playerManager.EnemyHit(col.gameObject, attackScript.damage);
+    {
+        if(attackScript.timeLeftToAttack >= attackScript.attackRate)
+		{
+			timeLeftToAttack = 0;
+        	Attack attackScript = this.GetComponent<Attack>();
+        	playerManager.EnemyHit(col.gameObject, attackScript.damage);
+		}
         if (this.CompareTag("Projectile") && !col.gameObject.CompareTag("Projectile"))
         {
-            gameObject.SetActive(false);
-            Destroy(gameObject);
+        	gameObject.SetActive(false);
+        	Destroy(gameObject);
         }
     }
 }
