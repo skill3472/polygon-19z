bae7f0924bb3014c20e5f384e20efc3f1df77244
Adam Madela <dungald@gmail.com>
2020-01-05 09:45:49 +0100

    tekst mniej sie rozmywa

--- roguelike_game/Assets/Scripts/UI/InventoryWindowManager.cs
+++ roguelike_game/Assets/Scripts/UI/InventoryWindowManager.cs
@@ -1,11 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class InventoryWindowManager : MonoBehaviour
 {
+    [SerializeField] private GameObject[] items;
+
     public void DestroySelf()
     {
         Destroy(gameObject);
+    }
+
+    public void AddItem(GameObject obj)
+    {
+
+
+        Transform scrollView = gameObject.transform.Find("Scroll View");
+        foreach (GameObject item in items)
+        {
+            Instantiate(item, scrollView, false);
+        }
     }
 }
